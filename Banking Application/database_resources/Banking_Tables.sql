631c53707f33a77c9b766ef864a1fde1e23f2e4e
trgreenman88 <trgreenman88@gmail.com>
2021-04-25 15:19:09 -0700

    Add files via upload
    Committed changes for Banking Application

---
+++ Banking Application/database_resources/Banking_Tables.sql
@@ -0,0 +1,35 @@
+create database Banking_Application;
+
+create table Users(
+	UserID smallserial,
+	Username varchar(100),
+	Password varchar(100),
+	constraint Users_pk primary key (UserID)
+);
+
+create table Account(
+	AccountID bigserial,
+	UserID smallserial,
+	Balance decimal,
+	constraint Account_pk primary key (AccountID),
+	constraint Account_fk foreign key (UserID) references Users(UserID)
+);
+
+create table Pending_Accounts(
+	AccountID bigserial,
+	UserID smallserial,
+	Status varchar(100), /*Approved, Rejected, or Pending*/
+	constraint Pending_Accounts_pk primary key (AccountID),
+	constraint Pending_Accounts_fk foreign key (UserID) references Users(UserID)
+);
+
+create table Transactions(
+	TransactionID bigserial,
+	AccountID bigserial,
+	UserID smallserial,
+	Date TimeStamp,
+	Description varchar(200),
+	Amount decimal, /*+ for getting money, - for giving money*/
+	constraint Transactions_pk primary key (TransactionID),
+	constraint Transactions_fk foreign key (AccountID) references Account(AccountID)
+);
